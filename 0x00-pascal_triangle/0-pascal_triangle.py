7184ac55c5e41161c9b8c8bdc26ef110ecd858d0
Tonyrealzy <umehobiarinze2@gmail.com>
2024-04-09 14:41:09 +0100

    Task completed!

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+def pascal_triangle(n):
+    """This function returns a list of lists representing Pascal's triangle of n rows.
+    
+    Args:
+    n: An integer representing the number of rows in the Pascal's triangle.
+    
+    Returns:
+    A list of lists representing Pascal's triangle. Returns an empty list if n <= 0.
+    """
+    
+    if n <= 0:
+        return []
+    
+    # Base case; first row is always [1]
+    triangle = [[1]]
+    
+    # Building the triangle row by row
+    for i in range(1, n):
+        previous_row = triangle[i-1]
+        current_row = triangle[1]
+        for j in range(1, i):
+            current_row.append(previous_row[j-1] + previous_row[j])
+        # End each row with 1
+        current_row.append(1)
+        triangle.append(current_row)
+        
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+### This directory contains the code files for defining the Pascal's Triangle theorem.
