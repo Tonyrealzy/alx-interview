b5498db1e12682ecb15583c84c64ae49257291d1
Tonyrealzy <umehobiarinze2@gmail.com>
2024-04-12 09:25:39 +0100

    Made new changes for test cases where n is greater than 5

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,15 +10,18 @@ def pascal_triangle(n):
     Returns:
     A list of lists representing Pascal's triangle. Returns an empty list if n <= 0.
     """
-    triangle = []
+    
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]
+    if n > 1:
+        triangle.append([1, 1])
     
     # Building the triangle row by row
-    for i in range(n):
-        row = []
-        for j in range(i + 1):
-            if (j == 0) or (j == 1):
-                row.append(1)
-            else:
-                row.append(triangle[i -1][j] + triangle[i - 1][j - 1])
-        triangle.append(row)
+    for i in range(2, n):
+        prev_row = triangle[i-1]
+        current_row = [sum(pair) for pair in zip([0] + prev_row, prev_row + [0])]
+        triangle.append(current_row)
+        
     return triangle
