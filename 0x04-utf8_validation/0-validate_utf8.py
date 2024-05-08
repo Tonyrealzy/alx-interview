79cf22992becd7f3f1d846398fa3a36b5a175020
Tonyrealzy <umehobiarinze2@gmail.com>
2024-05-08 09:28:29 +0100

    Completed the 0x04-utf8_validation task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,7 +2,6 @@
 """A Python script that determines if a given data set represents a valid UTF-8 encoding."""
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding."""
 
     num_bytes = 0
 
