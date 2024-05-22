5705c15d18bf4dcbb4f9aae6723d072dfcbd48fd
Tonyrealzy <umehobiarinze2@gmail.com>
2024-05-22 21:44:10 +0100

    Wrote the JS script

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,19 +4,21 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
     if (error) {
       console.error(error);
+      return;
     } else {
       const movieData = JSON.parse(body);
-      const characterUrls = movieData.characters;
+      const characters = movieData.characters;
   
-      characterUrls.forEach((characterUrl) => {
+      characters.forEach((characterUrl) => {
         request(characterUrl, (error, response, body) => {
           if (error) {
             console.error(error);
+            return;
           } else {
             const characterData = JSON.parse(body);
             process.stdout.write(characterData.name + '\n');
