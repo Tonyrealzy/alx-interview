482f398fa0a4cb2ce7329ea4955b9c05fb2d3f53
Tonyrealzy <umehobiarinze2@gmail.com>
2024-06-12 15:20:29 +0100

    Made a few modifications

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,7 +6,10 @@ that returns the perimeter of the island described in grid
 
 
 def island_perimeter(grid):
-    '''returns the perimeter of the island described in grid'''
+    """
+    returns the perimeter of the island described in grid
+    """
+
     counter = 0
     grid_max = len(grid) - 1  # index of the last list in the grid
     lst_max = len(grid[0]) - 1  # index of the last square in list
